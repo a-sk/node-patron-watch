b6a786100a0bb1eb51f459a9be29cc3a72a71834
Alexandr Skurikhin <a.skurihin@gmail.com>
2012-10-19 07:56:52 +0400

    Fix debug header. Code now is domain ready

--- index.js
+++ index.js
@@ -2,65 +2,67 @@ var chokidar = require('chokidar')
   , fs = require('fs');
 
 try {
-  var debug = require('debug')('patron')
+  var debug = require('debug')('patron-watch')
 } catch (e) {
   // production
   var debug = function() {}
 }
 
 
-function watcherPlugin(watcher, proxyTable) {
+function watcherPlugin(path, proxyTable) {
 
-    // object to assosiate files and rules
-    var filesRules = {};
+  debug('create watcher for %s', path)
+  var watcher = chokidar.watch(path, {ignored: /^\./, persistent: true});
 
-    watcher.on('add', function(path) {
-      debug('Add event')
-      var newProxyRules = require(path)
-      // copy properties from newProxyRules to proxyTable
-      for (var ruleName in newProxyRules) {
-        debug('Adding rule for %s', ruleName)
-        var rule = {}
-        rule[ruleName] = newProxyRules[ruleName]
-        proxyTable.emit('add', rule)
+  // object to assosiate files and rules
+  var filesRules = {};
 
-        if (!filesRules[path]) {
-          filesRules[path] = [ruleName]
-        } else {
-          filesRules[path].push(ruleName)
-        }
+  watcher.on('add', function(path) {
+    debug('Add event')
+    var newProxyRules = require(path)
+    // copy properties from newProxyRules to proxyTable
+    for (var ruleName in newProxyRules) {
+      debug('Adding rule for %s', ruleName)
+      var rule = {}
+      rule[ruleName] = newProxyRules[ruleName]
+      proxyTable.emit('add', rule)
+
+      if (!filesRules[path]) {
+        filesRules[path] = [ruleName]
+      } else {
+        filesRules[path].push(ruleName)
       }
+    }
+  })
+
+  watcher.on('change', function(path){
+    if (!fs.existsSync(path)) { return }
+    debug('Change event')
+    filesRules[path].forEach(function(ruleName) {
+      debug('Deleting rule for %s', ruleName)
+      proxyTable.emit('remove', ruleName)
     })
+    debug('Deleting filesRules %s', filesRules[path])
+    delete filesRules[path]
 
-    watcher.on('change', function(path){
-      if (!fs.existsSync(path)) { return }
-      debug('Change event')
-      filesRules[path].forEach(function(ruleName) {
-        debug('Deleting rule for %s', ruleName)
-        proxyTable.emit('remove', ruleName)
-      })
-      debug('Deleting filesRules %s', filesRules[path])
-      delete filesRules[path]
+    debug('Invalidating cache for %s', require.cache[require.resolve(path)])
+    delete require.cache[require.resolve(path)]
 
-      debug('Invalidating cache for %s', require.cache[require.resolve(path)])
-      delete require.cache[require.resolve(path)]
+    debug('Emiting add event')
+    watcher.emit('add', path)
+  })
 
-      debug('Emiting add event')
-      watcher.emit('add', path)
+  watcher.on('unlink', function(path) {
+    debug('unlink event')
+    filesRules[path].forEach(function(ruleName){
+      debug('Deleting rule for %s', ruleName)
+      proxyTable.emit('remove', ruleName)
     })
-
-    watcher.on('unlink', function(path) {
-      debug('unlink event')
-      filesRules[path].forEach(function(ruleName){
-        debug('Deleting rule for %s', ruleName)
-        proxyTable.emit('remove', ruleName)
-      })
-    })
-  }
+  })
+}
 
 module.exports = function(path) {
-  var watcher = chokidar.watch(path, {ignored: /^\./, persistent: true});
-  debug('create watcher for %s', path)
-  return function(e) {return watcherPlugin(watcher, e)}
+  if(!fs.existsSync(path)) {return}
+  return function(e) {return watcherPlugin(path, e)}
 }
 
